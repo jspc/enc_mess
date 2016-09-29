79dae30c726068aedf1bcbbcc4fd43983ebe9b7f
jspc <james@zero-internet.org.uk>
2016-09-29 17:27:01 +0100

    Init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+
+    "golang.org/x/crypto/ssh/terminal"
+)
+
+var password []byte
+var privateKeyPath string
+var publicKeyPath string
+
+func init() {
+    flag.StringVar(&privateKeyPath, "private-key", "./key.pem", "RSA Secret key PEM file")
+    flag.StringVar(&publicKeyPath, "public-key", "./key.pub", "RSA Public key PEM file")
+    flag.Parse()
+
+    fmt.Print("password: ")
+    bytePassword,_ := terminal.ReadPassword(0)
+
+    password = bytePassword
+}
+
+func main() {
+    plaintext := []byte("Plain text message to be encrypted")
+
+    encrypted := encrypt(plaintext)
+    decrypted := decrypt(encrypted)
+
+    fmt.Printf("OAEP Encrypted [%s] to \n[%x]\n", string(plaintext), encrypted)
+    fmt.Printf("OAEP Decrypted [%x] to \n[%s]\n", encrypted, decrypted)
+
+    return
+}
---
+++ rsa.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "crypto/md5"
+    "crypto/rand"
+    "crypto/rsa"
+    "crypto/x509"
+    "encoding/pem"
+    "io/ioutil"
+    "log"
+
+)
+
+var block *pem.Block
+var err error
+var pemData,label []byte
+var privateKey *rsa.PrivateKey
+var publicKey *rsa.PublicKey
+
+func ConfigureRSA() {
+    configurePrivateKey()
+    configurePublicKey()
+}
+
+func configurePublicKey() {
+    if pemData, err = ioutil.ReadFile(publicKeyPath); err != nil {
+        log.Fatalf("Error reading pem file: %s", err)
+    }
+
+    block, _ = pem.Decode(pemData)
+    if block == nil {
+        panic("failed to parse PEM block containing the public key")
+    }
+
+    publicKey, err = x509.ParsePKIXPublicKey(block.Bytes)
+    if err != nil {
+        panic("failed to parse DER encoded public key: " + err.Error())
+    }
+}
+
+func configurePrivateKey() {
+    if pemData, err = ioutil.ReadFile(privateKeyPath); err != nil {
+        log.Fatalf("Error reading pem file: %s", err)
+    }
+
+    if block, _ = pem.Decode(pemData); block == nil || block.Type != "RSA PRIVATE KEY" {
+        log.Fatal("No valid PEM data found")
+    }
+
+    s, _ := x509.DecryptPEMBlock(block, password)
+
+    if privateKey, err = x509.ParsePKCS1PrivateKey(s); err != nil {
+        log.Fatalf("Private key can't be decoded: %s", err)
+    }
+
+}
+
+func encrypt(plaintext []byte) (encrypted []byte) {
+    md5Hash := md5.New()
+
+    if encrypted, err = rsa.EncryptOAEP(md5Hash, rand.Reader, publicKey, plaintext, label); err != nil {
+        log.Fatal(err)
+    }
+    return
+}
+
+func decrypt(ciphertext []byte) (decrypted []byte) {
+    md5Hash := md5.New()
+
+    if decrypted, err = rsa.DecryptOAEP(md5Hash, rand.Reader, privateKey, ciphertext, label); err != nil {
+        log.Fatal(err)
+    }
+    return
+}
